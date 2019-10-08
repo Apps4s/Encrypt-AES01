d7bf741f9813f77d09d70ff4f31b53987a37fc62
Henry AT <yiperu@gmail.com>
2019-10-07 22:46:37 -0300

    Complete the proccess, now we make the desencrytp

--- app/src/main/java/com/example/encryptionaes/MainActivity.java
+++ app/src/main/java/com/example/encryptionaes/MainActivity.java
@@ -13,7 +13,7 @@ import javax.crypto.spec.SecretKeySpec;
 public class MainActivity extends AppCompatActivity {
 
   private String messageToEncrypt = "This is my personal message";
-  private static final String AES = "AES"
+  private static final String AES = "AES";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +30,28 @@ public class MainActivity extends AppCompatActivity {
 
   public static String encrypterAES(String messageToEncrypt) throws Exception {
 
+
+    // En esta parte encriptamos:
     KeyGenerator keyGenerator = KeyGenerator.getInstance(AES);
-    keyGenerator.init(128);
-    SecretKey secretKey = keyGenerator.generateKey();
-    byte[] bytesSecretKey = secretKey.getEncoded();
-    SecretKeySpec secretKeySpec = new SecretKeySpec(bytesSecretKey, AES);
-    Cipher cipher = Cipher.getInstance(AES);
-    cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-    byte[] encrytedMessage = cipher.doFinal(messageToEncrypt.getBytes());
+    keyGenerator.init(128); // Tamaño del generado
+    SecretKey secretKey = keyGenerator.generateKey(); // Instancia de llave secreta
+    byte[] bytesSecretKey = secretKey.getEncoded(); // Codificar la llave en bytes
+    SecretKeySpec secretKeySpec = new SecretKeySpec(bytesSecretKey, AES); // Construir una clave secreta indicandole que es de tipo AES
+    Cipher cipher = Cipher.getInstance(AES); // Intanciamos un objeto de cifrado de tipo AES
+    cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec); // Inicializamos el sistema de cifrado en modo encriptacion
+    byte[] encrytedMessage = cipher.doFinal(messageToEncrypt.getBytes()); // procedemos a encriptar el mensaje
+    Log.d("TAG", "======================");
     Log.d("TAG", new String(encrytedMessage));
-    return new String(encrytedMessage);
+    Log.d("TAG", "======================");
+    // Enviamos el mensaje a otro dipositivo
+
+    // En esta parte Desencriptamos:
+    cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+    byte[] mensajeDesEncriptado = cipher.doFinal(encrytedMessage);
+    Log.d("TAG", "* * * * * * * * * * * * * * * *");
+    Log.d("TAG", new String(mensajeDesEncriptado));
+    Log.d("TAG", "* * * * * * * * * * * * * * * *");
+    return new String(mensajeDesEncriptado);
   }
 
 
